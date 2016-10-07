b480e3fb027d18788453af7cbc807851cc099373
John <johnmstash@gmail.com>
2016-10-06 22:01:54 -0400

    Using global promise in mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,9 @@
 var config = require('./config'),
     mongoose = require('mongoose');
 
+// Avoids warning regarding deprected mpromise library.
+ mongoose.Promise = global.Promise;
+
 module.exports = function() {
 
     var db = mongoose.connect(config.db);
