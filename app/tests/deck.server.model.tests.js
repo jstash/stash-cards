3ea542dd1db9f45c2d2d09d3fb4ea664b0701558
John <johnmstash@gmail.com>
2016-10-08 16:06:02 -0400

    finished inital set of deck model unit tests.

--- app/tests/deck.server.model.tests.js
+++ app/tests/deck.server.model.tests.js
@@ -35,10 +35,67 @@ describe('Deck Model Unit Tests:', function() {
 
     describe('Testing the save method', function () {
 
-        it('Should be able to save without problems', function (done) {
+        it('Should be able to save a new deck', function (done) {
             deck.save(function (err) {
                 should.not.exist(err);
                 done();
+            });
+        });
+
+        it('Should be able to add a card to a deck', function (done) {
+            
+            deck.cards.should.have.length(2);
+
+            var card  = {
+                front: 'front 2',
+                back: 'back 2',
+                reviewCount: 0,
+                correctCount: 0
+            };
+            deck.save(function (err) {
+                should.not.exist(err);
+            });
+            deck.cards.push(card);
+            deck.save(function (err) {
+                should.not.exist(err);
+            });
+            deck.cards.should.have.length(3);
+            done();
+        });
+
+        it('Should be able to remove a card from a deck', function (done) {
+
+            deck.save(function (err) {
+                should.not.exist(err);
+                deck = Deck.findOne(function(err, mydeck) {
+                    should.not.exist(err);
+                    should(mydeck.cards).have.length(2);
+
+                    mydeck.cards.splice(1,1);
+                    mydeck.save(function (err) {
+                        should.not.exist(err);
+                    });
+                    mydeck.cards.should.have.length(1);
+                    done();
+                });          
+            });  
+
+
+        });
+
+        it('Should be able to delete a deck', function (done) {
+            deck.save(function (err) {
+                should.not.exist(err);
+                Deck.findOne(function(err, myDeck) {
+                    should.not.exist(err);
+                    myDeck.remove(function(err) {
+                        should.not.exist(err);
+                        Deck.count(function (err, c) {
+                            should(c).be.equal(0);
+                            done();
+                        });
+                    });
+                });
             });
         });
     });
